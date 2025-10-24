5cc81174a6d2eacd63fd75ff801c3c28a7b0e6e7
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-10-23 20:13:18 -0400

    Wiring up ValidateForm along with some safegaurds to prevent incorrect validation for the CreatePrizeForm

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary;
 
 namespace TrackerUI
 {
@@ -19,7 +20,14 @@ namespace TrackerUI
 
         private void createPrizeButton_Click(object sender, EventArgs e)
         {
-
+            if (ValidateForm())
+            {
+                PrizeModel model = new PrizeModel(
+                    placeNameValue.Text,
+                    placeNumberValue.Text,
+                    prizeAmountValue.Text,
+                    prizePercentageValue.Text);
+            }
         }
 
         private bool ValidateForm()
@@ -29,6 +37,37 @@ namespace TrackerUI
             bool placeNumberValidNumber = int.TryParse(placeNumberValue.Text, out placeNumber);
 
             if (placeNumberValidNumber == false)
+            {
+                output = false;
+            }
+
+            if (placeNumber < 1)
+            {
+                output = false;
+            }
+
+            if (placeNameValue.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeAmount = 0;
+            double prizePercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(prizeAmountValue.Text, out prizeAmount);
+            bool prizePercentageValid = double.TryParse(prizeAmountValue.Text, out prizePercentage);
+
+            if (prizeAmountValid == false || prizePercentageValid == false)
+            {
+                output = false;
+            }
+
+            if (prizeAmount <= 0 && prizePercentage <= 0)
+            {
+                output = false;
+            }
+
+            if (prizePercentage < 0 || prizePercentage > 100)
             {
                 output = false;
             }
